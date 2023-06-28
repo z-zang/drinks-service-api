7334e1a1041ed678e9439899a58634d261a945d7
Zichao Zang <zang.code@gmail.com>
2023-06-28 01:32:15 +0100

    feat: Add additional tests for coffee controller

--- src/controllers/coffee_controller.test.ts
+++ src/controllers/coffee_controller.test.ts
@@ -1,15 +1,43 @@
 import request from 'supertest';
 import { app } from '../app';
 
-test('GET /coffee should return correct object', async () => {
-    const res = await request(app)
+describe('GET /coffee should return correct object', () => {
+    it('should return default coffee name for unspecified query', async () => {
+        const res = await request(app)
+        .get('/coffee')
+
+        expect(res.statusCode).toEqual(200);
+
+        expect(res.body).toEqual({
+            drinkType: 'Coffee',
+            name: 'Latte',
+        });
+    })
+    
+    it('should return query name for coffee name for specified query', async () => {
+        const res = await request(app)
+        .get('/coffee')
+        .query({ coffeeName: 'cappucino' });
+
+        expect(res.statusCode).toEqual(200);
+
+        expect(res.body).toEqual({
+            drinkType: 'Coffee',
+            name: 'cappucino',
+        });
+    })
+
+    it('should return query name for coffee name for specified query', async () => {
+        const res = await request(app)
         .get('/coffee')
         .query({ coffeeName: 'Latte' });
 
-    expect(res.statusCode).toEqual(200);
+        expect(res.statusCode).toEqual(200);
 
-    expect(res.body).toEqual({
-        drinkType: 'Coffee',
-        name: 'Latte',
-    });
+        expect(res.body).toEqual({
+            drinkType: 'Coffee',
+            name: 'Latte',
+        });
+    })
+
 });
